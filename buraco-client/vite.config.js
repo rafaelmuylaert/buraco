4ed9112a48eb204c3a17043b9c51cd9dd9cc902e
Rafael Muylaert <6547337+rafaelmuylaert@users.noreply.github.com>
2026-03-09 19:40:56 -0400

    change server port in vite.config

--- buraco-client/vite.config.js
+++ buraco-client/vite.config.js
@@ -10,7 +10,12 @@ export default defineConfig({
   server: {
     allowedHosts: true,
     port: parseInt(process.env.CLIENT_PORT || '5173'),
-    host: true
+    host: true,
+    proxy: {
+      '/buraco/api': { target: `http://buraco-server:${process.env.SERVER_PORT || '8000'}`, rewrite: path => path.replace(/^\/buraco/, '') },
+      '/buraco/games': { target: `http://buraco-server:${process.env.SERVER_PORT || '8000'}`, rewrite: path => path.replace(/^\/buraco/, '') },
+      '/buraco/socket.io': { target: `http://buraco-server:${process.env.SERVER_PORT || '8000'}`, ws: true, rewrite: path => path.replace(/^\/buraco/, '') }
+    }
   },
   preview: {
     allowedHosts: true,
