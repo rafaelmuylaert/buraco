c012228ab667d120740961cd8d68aa16edf9c978
Rafael Muylaert <6547337+rafaelmuylaert@users.noreply.github.com>
2026-03-17 08:04:58 -0400

    change url path

--- buraco-client/vite.config.js
+++ buraco-client/vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/buraco/', // THIS IS CRITICAL FOR NGINX SUB-PATHS
-  
-  // THIS is the block that 'npm run dev' uses!
+  base: '/',
   server: {
-    allowedHosts: true, // Trusts your Nginx proxy hostname
+    allowedHosts: true,
     port: 5173,
     host: true
   },
-
-  // (Optional) Kept here just in case you ever run 'npm run preview' again
   preview: {
-    allowedHosts: true, 
+    allowedHosts: true,
     port: 5173,
     host: true
   }
