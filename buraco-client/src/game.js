31a02fc7187032663968b2a88053925812b1d558
Rafael Muylaert <6547337+rafaelmuylaert@users.noreply.github.com>
2026-05-16 15:18:20 -0400

    fix playing new sequences

--- buraco-client/src/game.js
+++ buraco-client/src/game.js
@@ -386,10 +386,10 @@ export function parseMeld(cardIds, rules, existingMeld = null, meldSuit = 0) {
     let suitrank = null;
     if(!existingMeld) {
         suitrank = newsuitorrank(cardIds);
-        console.log("[GAME.JS] New game: ",newsuitorrank);
+        console.log("[GAME.JS] New game: ",suitrank);
         if(!suitrank === null) {console.log("[GAME.JS] INVALID MOVE: Suitrank returned null"); return null;}
         if(suitrank.rank !== null) return cardsToRunnerSlots(cardIds, null, rules);
-        else if(suitrank.suit !== null) cardsToSeqSlots(cardIds, null, suitrank.suit);
+        else if(suitrank.suit !== null) return cardsToSeqSlots(cardIds, null, suitrank.suit);
         console.log("[GAME.JS] INVALID MOVE: new meld failed"); return null;
     }
     else if (!isSeq(existingMeld)) return cardsToRunnerSlots(cardIds, existingMeld, rules);
