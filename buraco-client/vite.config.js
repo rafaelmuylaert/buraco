86f81f864f0796e37c2a61f2262cb72edeb0f1ae
Rafael <rafael@rafamano.com>
2026-08-11 19:20:12 -0400

    Update buraco-client/vite.config.js

--- buraco-client/vite.config.js
+++ buraco-client/vite.config.js
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+ // 1. Force Vite to pre-bundle the core sub-export
+  optimizeDeps: {
+    include: ['boardgame.io/core']
+  },
+  
+  // 2. Tell Rollup to allow CommonJS translation for boardgame.io
+  build: {
+    commonjsOptions: {
+      include: [/boardgame\.io/, /node_modules/]
+    }
+  },
   base: '/',
   server: {
     allowedHosts: true,
